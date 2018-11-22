4a67628651a84cab25048ce8e273daafdf0e371c
Anaïs <anais.diologeant@hotmail.fr>
2018-11-22 17:03:39 +0100

    Fonction supprimer email

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root to: "email#index"
   resources :tasks, except: [:show]
   get 'email/show', to: 'email#show'
+  delete 'email/delete', to: 'email#destroy' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
