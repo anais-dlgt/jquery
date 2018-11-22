dc5d558d7a1c1cd0f277ed9dcc4eec8eaa7827f4
Anaïs <anais.diologeant@hotmail.fr>
2018-11-22 22:33:10 +0100

    Dernier c'est promis

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,19 +1,25 @@
 class EmailController < ApplicationController
 
   def index
-    @email = Email.all
+    @email = Email.all.order("created_at DESC")
   end
 
   def show
     @email = Email.find(params[:id])
+    @email.read = true
+    @email.save
     respond_to do |format|
       format.js
     end
   end
 
   def destroy
-    @email = Email.destroy(params[:id])
-    redirect_to root_path
+    @email = Email.find(params[:id])
+    @email.destroy
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'email/index'
-  get 'home/index'
   devise_for :users
   root to: "email#index"
   resources :tasks, except: [:show]
-  get 'email/show', to: 'email#show'
-  delete 'email/delete', to: 'email#destroy' 
+  resources :email
 end
